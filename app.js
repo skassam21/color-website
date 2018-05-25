35ba929d19bac49f686d2f95580855eaaa7e0bf4
Shums Kassam <skassam21@gmail.com>
2018-05-25 11:20:36 -0400

    added functionality

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ function calculateRGB() {
     let greenValue = 0;
 
 
+    let redHexValue =hexValue.substring(0, 2);
+    console.log(redHexValue);
+    let blueHexValue = hexValue.substring(2, 4);
+    console.log(blueHexValue);
+    let greenHexValue = hexValue.substring(4, 6);
+    console.log(greenHexValue); 
+
+    redValue = parseInt(redHexValue, 16);
+    blueValue = parseInt(blueHexValue, 16);
+    greenValue = parseInt(greenHexValue, 16);
 
     // DO NOT DELETE BELOW
     document.getElementById("rgb-value").value = redValue + ", " + blueValue + ", " + greenValue;
